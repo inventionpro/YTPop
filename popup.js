68a3ef79f0e5d45a505a64cd6932bde56e0b954f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-04-18 18:36:24 +0200

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,31 @@
+function set(name) {
+  chrome.storage.sync.get(name, function (data) {
+    document.getElementById(name).checked = data[name];
+  });
+  document.getElementById(name).addEventListener('change', function () {
+    let da = {};
+    da[name] = document.getElementById(name).checked
+    chrome.storage.sync.set(da);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  ['enabled','down','noads','nocurves','noshorts','nothanks','nomix'].forEach(e => {
+    set(e)
+  })
+  
+  chrome.storage.onChanged.addListener(function (changes, namespace) {
+    for (let key in changes) {
+      console.log(key)
+      document.getElementById(key).checked = changes[key].newValue;
+    }
+  })
+  
+  var manifestData = chrome.runtime.getManifest();
+  document.getElementById('ver').innerHTML = 'v'+manifestData.version+' m'+manifestData.manifest_version;
+  chrome.commands.getAll((commands) => {
+    commands.forEach((command) => {
+      if (command.shortcut) document.querySelector('label[for="'+command.name+'"]').insertAdjacentHTML('afterend', command.shortcut.split('+').map(key => key.trim()).map(e=>{return `<kbd>${e}</kbd>`}).join('+'))
+    });
+  });
+});
